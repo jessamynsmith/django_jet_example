810bc6be1b24d01b019aadefba04b1b045271cb9
jessamynsmith <jessamyn.smith@gmail.com>
2018-05-24 23:51:45 -0500

    Set up app for heroku deploy

--- django_jet_example/settings.py
+++ django_jet_example/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 
 import os
 
+import django_heroku
+
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -25,7 +28,7 @@ SECRET_KEY = '5^$of*u05pvq7q^f1#!!s4ypstphcin^kd_k3h8$bc5$hbj4li'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0:5000', '127.0.0.1:8000', 'django-jet-example.herokuapp.com']
 
 
 # Application definition
@@ -49,8 +52,6 @@ INSTALLED_APPS = [
 
 SITE_ID = 1
 
-JET_INDEX_DASHBOARD = 'django_jet_example.dashboard.CustomIndexDashboard'
-
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -59,6 +60,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'django_jet_example.urls'
@@ -130,3 +132,17 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+JET_INDEX_DASHBOARD = 'django_jet_example.dashboard.CustomIndexDashboard'
+
+REST_FRAMEWORK = {
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ]
+}
+
+
+django_heroku.settings(locals())
