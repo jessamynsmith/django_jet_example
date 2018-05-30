9518f5e6a89bab8fc4cbadd4842580579da0a7ac
jessamynsmith <jessamyn.smith@gmail.com>
2018-05-29 23:27:34 -0600

    Added tags by hour chart.

--- people/dashboard.py
+++ people/dashboard.py
@@ -1,5 +1,9 @@
+from collections import OrderedDict
+import datetime
+
 from django.db.models import Count
 from django.utils.translation import ugettext_lazy as _
+from django.utils import timezone
 from jet.dashboard import modules
 from jet.dashboard.dashboard import Dashboard
 from jet.dashboard.modules import DashboardModule
@@ -34,12 +38,45 @@ class TagPopularityChart(DashboardModule):
             count = count + 1
 
 
+class TagsOverTimeChart(DashboardModule):
+    """
+    Chart.js widget that shows tags by hour
+    """
+
+    title = _('Tags by hour')
+    template = 'dashboard/modules/tags_over_time_chart.html'
+    style = 'overflow-x: auto;'
+    hours = 24
+
+    class Media:
+        js = (
+            'people/tags_over_time_chart.js',
+        )
+
+    def init_with_context(self, context):
+        start_time = timezone.now() - datetime.timedelta(hours=self.hours)
+        result = people_models.PersonTag.objects.filter(created_at__gt=start_time).extra(
+            select={'hour': "date_part(\'hour\', \"created_at\")"}).values('hour').order_by('hour')
+
+        results_dict = OrderedDict()
+        for hour in range(1, self.hours + 1):
+            current = start_time + datetime.timedelta(hours=hour)
+            results_dict[int(current.hour)] = 0
+
+        for data in result:
+            results_dict[int(data['hour'])] += 1
+
+        for key, value in results_dict.items():
+            self.children.append((key, value))
+
+
 class CustomIndexDashboard(Dashboard):
     columns = 2
 
     def init_with_context(self, context):
         self.available_children.append(modules.LinkList)
         self.available_children.append(TagPopularityChart)
+        self.available_children.append(TagsOverTimeChart)
 
         self.children.append(modules.LinkList(
             _('Resources'),
@@ -74,6 +111,12 @@ class CustomIndexDashboard(Dashboard):
             ],
             column=0,
             order=1
+        ))
+
+        self.children.append(TagsOverTimeChart(
+            _('Tags By Hour'),
+            column=0,
+            order=2
         ))
 
         self.children.append(TagPopularityChart(
---
+++ people/static/people/tags_over_time_chart.js
@@ -0,0 +1,61 @@
+(function ($) {
+    $.fn.extend( {
+        tagsOverTimeChart: function() {
+            var $chart = $(this);
+            var $data = $chart.find('.chart-data');
+            var $dataItems = $data.find('.chart-data-item');
+            var labels = [];
+            var data = [];
+
+            $dataItems.each(function() {
+                labels.push($(this).data('tag'));
+                data.push($(this).data('count'));
+            });
+
+            var chart = new Chart(document.getElementById("chart_tags_over_time"), {
+                type: 'bar',
+                data: {
+                  labels: labels,
+                  datasets: [
+                    {
+                      label: "Count",
+                      backgroundColor: $chart.find('.chart-fillColor').css('color'),
+                      borderColor	: $chart.find('.chart-strokeColor').css('color'),
+                      borderWidth: 2,
+                      responsive: true,
+                      data: data
+                    }
+                  ]
+                },
+                options: {
+                  legend: { display: false },
+                  title: { display: false },
+                  scales: {
+                    yAxes: [{
+                        ticks: {
+                            beginAtZero: true,
+                            stepSize: 1,
+                        }
+                    }]
+                  }
+                }
+            });
+
+            var updateChartColors = function(chart) {
+                for (var i = 0; i < chart.datasets.length; ++i) {
+                    chart.datasets[i]['fillColor'] = $chart.find('.chart-fillColor').css('color');
+                    chart.datasets[i]['strokeColor'] = $chart.find('.chart-strokeColor').css('color');
+                }
+
+                chart.update();
+            };
+
+            $(document).on('theme:changed', function() {
+                updateChartColors(chart);
+            });
+        }
+    });
+
+
+
+})(jet.jQuery);
